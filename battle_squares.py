8372eca450ae02312870384346d64b6dd23902a3
Kevin Yaiko <itd07@yahoo.com>
2017-04-18 15:29:55 +0200

    Changes player shapes to squares from circles. Adds functions to keep squares from going off screen top and bottom.

--- battle_squares.py
+++ battle_squares.py
@@ -8,78 +8,69 @@ from pygame.locals import *
 FPS = 30
 WINDOWWIDTH = 1024
 WINDOWHEIGHT = 760
-RADIUS = 15
-MOVE = 5
+PLAYERSIZE = 30
+MOVE = 20
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 BLUE = (0,0,255)
 
+player1rect = pygame.Rect(400,200,PLAYERSIZE,PLAYERSIZE)
+player2rect = pygame.Rect(800,300,PLAYERSIZE,PLAYERSIZE)
 
 
-def check_middle(x,y,windowwidth,windowheight,radius,move):
+def check_middle_1(player):
 	'''Checks to make sure the player can't cross center line'''
-	if x < windowwidth/2:
-		if x + radius + move < windowwidth/2:
-			return MOVE
-		else:
-			return 0
+	return None
+
+
+def check_top(player):
+	if player.top - MOVE >= 0:
+		return MOVE
 	else:
-		if x - radius - move > windowwidth/2:
-			print(x)
-			return MOVE
-		else:
-			return 0
+		return 0
+		
 
-
-def check_border(x,y,region,windowwidth,windowheight,radius,move):
-	if region == "left":
-		if x > radius + move:
-			return MOVE
-		else:
-			return 0
+def check_bottom(player):
+	if player.bottom + MOVE <= WINDOWHEIGHT:
+		return MOVE
+	else:
+		return 0
 
 
 def main():
 	pygame.init()
 	DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT)) #Can add pygame.RESIZABLE
 	pygame.display.set_caption('Battle Squares')
-	player1x = 400
-	player1y = 300
-	player2x = 600
-	player2y = 500
+	DISTANCE = MOVE
     
 	while True:
 		DISPLAYSURF.fill(WHITE)
 		pygame.draw.line(DISPLAYSURF, BLACK, (WINDOWWIDTH/2,0), (WINDOWWIDTH/2,WINDOWHEIGHT),3)
-		pygame.draw.circle(DISPLAYSURF, RED, (player1x,player1y), RADIUS)
-		pygame.draw.circle(DISPLAYSURF, BLUE, (player2x,player2y), RADIUS)
+		pygame.draw.rect(DISPLAYSURF, BLUE, player1rect)
+		pygame.draw.rect(DISPLAYSURF, RED, player2rect)
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
 			elif event.type == KEYUP:
 				if (event.key == K_RIGHT):
-					DISTANCE = check_middle(player1x, player1y, WINDOWWIDTH, WINDOWHEIGHT,RADIUS,MOVE)
-					player1x += DISTANCE
+					player1rect.right += check_middle_1(player1rect)
 				elif (event.key == K_LEFT):
-					DISTANCE = check_border(player1x, player1y,'left', WINDOWWIDTH, WINDOWHEIGHT,RADIUS,MOVE)
-					player1x -= DISTANCE
+					player1rect.left -= MOVE
 				elif (event.key == K_UP):
-					player1y -= DISTANCE
+					player1rect.top -= check_top(player1rect)
 				elif (event.key == K_DOWN):
-					player1y += DISTANCE  
+					player1rect.bottom += check_bottom(player1rect)
 				elif (event.key == K_s):
-					player2y += 5
+					player2rect.bottom += check_bottom(player2rect)
 				elif (event.key == K_w):
-					player2y -= 5
+					player2rect.top -= check_top(player2rect)
 				elif (event.key == K_a):
-					DISTANCE = check_middle(player2x, player2y, WINDOWWIDTH, WINDOWHEIGHT, RADIUS, MOVE)
-					player2x -= DISTANCE
+					player2rect.left -= 5
 				elif (event.key == K_d):
-					player2x += 5
-			DISTANCE = 5
+					player2rect.right += 5
 		pygame.display.update()
 
 main()
